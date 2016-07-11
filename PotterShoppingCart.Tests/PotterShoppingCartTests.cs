f2b6ca8adb614a50af28863f6aca8cf20818e77b
Elaine <elainran@gmail.com>
2016-07-11 23:29:20 +0800

    第一集買了一本，其他都沒買，價格應為100*1=100元(紅燈)

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod]
+        public void Buy_ep1_and_no_buy_other_should_be_100()
+        {
+            //arrange
+            var target = new CalculatorCart();
+
+            //act
+            var actual = target.Compute();
+
+            //assert
+            var expect = 100;
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
